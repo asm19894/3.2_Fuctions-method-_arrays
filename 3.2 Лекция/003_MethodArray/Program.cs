c99e2e1c7eea49641f4f95f0edf7a43cca7e17c7
Anna Smirnova <sanna_i@mail.ru>
2023-03-13 13:04:53 +0300

    002_MethodArray

---
+++ 3.2 Лекция/003_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {1, 2, 31, 14, 21, 16, 23, 4};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.Write (index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
+
